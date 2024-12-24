a0ec59a30606524eea9b38bf5e62532c3194fc03
vengersmoon <vengersmon003@gmail.com>
2024-12-24 15:00:40 +0530

    f

--- src/ArraysExample.java
+++ src/ArraysExample.java
@@ -8,6 +8,7 @@ public class ArraysExample {
         // int eng = 89;
         // length
         // sort
+        // casting
 
         // int [] marks = new int[4];
        
@@ -24,11 +25,24 @@ public class ArraysExample {
         // Arrays.sort(marks);
         // System.out.println("After sorting:" + marks[0]);
 
-        int [] marks = {97, 89, 88, 9};
+       // int [] marks = {97, 89, 88, 9};
         
-        int [][] finalMarks = {{2,3,5,6}, {90,89,78,76}};         // 2D array
-        System.out.println(finalMarks[1][2]);
+       // int [][] finalMarks = {{2,3,5,6}, {90,89,78,76}};         // 2D array
+       // System.out.println(finalMarks[1][2]);
+        
+        // casting
 
+        double price = 100.0;
+        double finalPrice = price+18;  // double can store int value and it is called Implicit casting
+
+        System.out.println(finalPrice);
+
+        // int p = 100;
+        // int Fp = p + 18.0;              // casting occur because a glass can fill a bucket but a bucket can't fill a glass
+
+        int p = 100;
+        int Fp = p + (int)18.0;          // by Explicit casting we can store double value in int
+        System.out.println(Fp);
 
     }
 }
---
+++ src/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+int [] marks = {97, 89, 88, 9};
+        
+        // int [][] finalMarks = {{2,3,5,6}, {90,89,78,76}};         // 2D array
+        // System.out.println(finalMarks[1][2]);
