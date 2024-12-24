545f1d1c88dcc3563aa1b230631a0a3986bb6e23
vengersmoon <vengersmon003@gmail.com>
2024-12-24 15:44:54 +0530

    fd

--- src/operate.java
+++ src/operate.java
@@ -3,5 +3,20 @@ public class operate {
     public static void main(String[] args) {
         
         // operators in java
+
+        // 1. Arithmetic operators
+        // 2. Assignment operators
+        // 3. Comparison operators
+        // 4. Logical operators
+        // 5. Bitwise operators
+
+
+        // 1. Arithmetic operators
+
+        int a = 1;
+        int b = 2;
+        int sum = a+b;
+        System.out.println(sum);
+
     }
 }
