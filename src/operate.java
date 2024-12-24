81fa001028bc77960e659994f31f43e9dcf8d651
vengersmoon <vengersmon003@gmail.com>
2024-12-25 00:00:01 +0530

    java practise

--- src/operate.java
+++ src/operate.java
@@ -11,12 +11,27 @@ public class operate {
         // 5. Bitwise operators
 
 
-        // 1. Arithmetic operators
+        // 1. Arithmetic operators  
 
-        int a = 1;
-        int b = 2;
-        int sum = a+b;
-        System.out.println(sum);
+        // int a = 1;
+        // int b = 2;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        // unary operators
+
+    //     int num = 1;
+    //     //num = num+1;
+    //    // num+=1;
+    //     num--;  // unary operator
+    //     System.out.println(num);
+
+
+        // Maths
+       // System.out.println(Math.min(5, 6));
+      //    System.out.println((int)(Math.random()*100));
+
+      
 
     }
 }
--- src/tempCodeRunnerFile.java
+++ src/tempCodeRunnerFile.java
@@ -1,4 +1,5 @@
-// int [] marks = {97, 89, 88, 9};
-        
-//         // int [][] finalMarks = {{2,3,5,6}, {90,89,78,76}};         // 2D array
-//         // System.out.println(finalMarks[1][2]);
+// int num = 1;
+    //     //num = num+1;
+    //    // num+=1;
+    //     num--;  // unary operator
+    //     System.out.println(num);
