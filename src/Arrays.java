427165f46c1743b26ed70f11090c3ae7d13b1249
vengersmoon <vengersmon003@gmail.com>
2024-12-24 13:44:46 +0530

    f

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,6 +2,10 @@ public class Arrays {
     
     public static void main(String[] args) {
         
-        
+        int age = 30;
+        int physics = 97;
+        int chem = 56;
+        int eng = 89;
+
     }
 }
