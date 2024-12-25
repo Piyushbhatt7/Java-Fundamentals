2f3dbd8d1b6ccc3bcccbc8e36aafcf0cc135db24
vengersmoon <vengersmon003@gmail.com>
2024-12-25 23:00:50 +0530

    ff

--- src/loops.java
+++ src/loops.java
@@ -2,7 +2,7 @@ public class loops {
     
     public static void main(String[] args) {
         
-        // loops in java
+        // loops in java f
        // 1. for loop
        // 2. while loop
        // 3. do-while loop
