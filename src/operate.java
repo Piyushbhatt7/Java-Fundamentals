8b169d22477f7d1e9f4c5b02620af29ee0b944ac
vengersmoon <vengersmon003@gmail.com>
2024-12-25 14:46:52 +0530

    gg

--- src/operate.java
+++ src/operate.java
@@ -99,7 +99,7 @@ public class operate {
       
     // Switch Statment
 
-    int day = 1; // 1- monday
+    int day = 1; // 1- monday 2- tuesday 3- wednesday 4- thursday 5- friday 6- saturday 7- sunday
 
      }
 
