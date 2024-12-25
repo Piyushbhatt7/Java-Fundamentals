34417eaa90c4b5008914357f2783fd28d2a18fe3
vengersmoon <vengersmon003@gmail.com>
2024-12-25 15:23:19 +0530

    gg

---
+++ src/calculator.java
@@ -0,0 +1,10 @@
+public class calculator{
+
+    public static void main(String[] args) {
+        
+        int num1, num2;
+        double sum;
+
+        System.out.println("Enter first number: ");
+    }
+}
