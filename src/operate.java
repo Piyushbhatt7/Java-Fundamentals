7b00743b3b39c4203366889fe8a45ba06dfd5499
vengersmoon <vengersmon003@gmail.com>
2024-12-25 20:04:39 +0530

    gg

--- src/operate.java
+++ src/operate.java
@@ -102,7 +102,23 @@ public class operate {
     int day = 1; // 1- monday 2- tuesday 3- wednesday 4- thursday 5- friday 6- saturday 7- sunday
 
     switch(day){
+        case 1: 
+        System.out.println("Monday");
+        case 2:
+        System.out.println("Tuesday");
+        case 3: 
+        System.out.println("Wednesday");
+        case 4:
+        System.out.println("Thursday");
+        case 5:
+        System.out.println("Friday");
+        case 6:
+        System.out.println("Saturday");
+        case 7:
+        System.out.println("Sunday");
         
+        default:
+        System.out.println("Invalid day");
     }
 
      }
