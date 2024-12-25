d13fe7ebc12cfe8e59ca3372b8f4cf9967d305f9
vengersmoon <vengersmon003@gmail.com>
2024-12-25 15:32:45 +0530

    f

--- src/calculator.java
+++ src/calculator.java
@@ -34,22 +34,27 @@ public class calculator{
             System.out.println("Difference of " + num1 + " and " + num2 + " is = " + sum);
         }
 
-        if(operator.equals("*")){
+        else if(operator.equals("*")){
 
             sum = num1 * num2;
             System.out.println("Multiplication of " + num1 + " and " + num2 + " is = " + sum);
         }
 
-        if(operator.equals("/")){
+        else if(operator.equals("/")){
 
             sum = num1 / num2;
             System.out.println("Division of " + num1 + " and " + num2 + " is = " + sum);
         }
 
-        if(operator.equals("%")){
+        else if(operator.equals("%")){
 
             sum = num1 % num2;
             System.out.println("Modulor of " + num1 + " and " + num2 + " is = " + sum);
+        }
+
+        else{
+
+            System.out.println("Wrong input!!");
         }
 
     }
