7df730e6bd41f4d81d6e1429ce619724867397dd
vengersmoon <vengersmon003@gmail.com>
2024-12-26 14:24:09 +0530

    kk

--- src/methods.java
+++ src/methods.java
@@ -1,17 +1,19 @@
 public class methods {
     
-    public static void printjava(){
-        System.out.println("Hello Java");
-    }
-    public  static  void printName(String name){
+    // public static void printjava(){
+    //     System.out.println("Hello Java");
+    // }
+    // public  static  void printName(String name){
 
-        System.out.println(name);
-    }
+    //     System.out.println(name);
+    // }
+
+
     public static void main(String[] args) {
         
         // Methods in Java
 
-        printName("Aman");
+        // printName("Aman");
 
     }
 }
