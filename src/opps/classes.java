252b8e083f6e88cc333a3e22449b4bd55dc281de
vengersmoon <vengersmon003@gmail.com>
2024-12-26 21:44:28 +0530

    f

--- src/opps/classes.java
+++ src/opps/classes.java
@@ -3,15 +3,20 @@ package opps;
 public class classes {
 
     public static void main(String[] args){
-
+               
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "Gel";
+        pen1.write();
     }
 
-    public class Pen{
+   
+    public static class Pen{
         String color;
         String type; // ballpoint, gel point
 
         public void write(){
-            System.out.println("Writimg something");
+            System.out.println("Writing something");
         }
     }
 }
