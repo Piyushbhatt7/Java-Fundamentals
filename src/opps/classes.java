6e250a95b2cbc7902c97b4e69fe9411f6e5a0a50
vengersmoon <vengersmon003@gmail.com>
2024-12-26 21:40:49 +0530

    f

--- src/opps/classes.java
+++ src/opps/classes.java
@@ -5,4 +5,11 @@ public class classes {
     public static void main(String[] args){
 
     }
+
+    public class Pen{
+        String color;
+        String type; // ballpoint, gel point
+
+        
+    }
 }
