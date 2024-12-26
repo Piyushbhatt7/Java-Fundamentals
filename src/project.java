42c036f84e62bdc630841f26d936f93d71e791c9
vengersmoon <vengersmon003@gmail.com>
2024-12-26 14:36:39 +0530

    ff

--- src/project.java
+++ src/project.java
@@ -1,17 +1,20 @@
 
-import java.util.Scanner;
 
 public class project {
     
     // Mini projects
 
     public static void main(String[] args){
-           
-        int myNum = (int)Math.random()*100;
+        
         Scanner sc = new Scanner(System.in);
+        int myNum = (int)Math.random()*100;
+    
+       
         do { 
-             
+              System.out.println("Guess my number: ");
+              int userNum = sc.nextInt();
+              
         } 
-        while (true);
+        while ();
     }
 }
