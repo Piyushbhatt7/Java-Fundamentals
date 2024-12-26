292b88eac420bcc4391d470d6cb3a9b8382ba599
vengersmoon <vengersmon003@gmail.com>
2024-12-26 22:02:31 +0530

    h

--- src/ArraysExample.java
+++ src/ArraysExample.java
@@ -2,7 +2,7 @@ public class ArraysExample {
     
     public static void main(String[] args) {
         
-        // int age = 30;
+        // int age = 30; 
         // int physics = 97;
         // int chem = 56;
         // int eng = 89;
