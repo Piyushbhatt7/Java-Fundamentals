75b588089f521ea84d865d8b991ccec881af78c1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:06:51 +0530

    using scanner class to take user input

--- src/Input.java
+++ src/Input.java
@@ -1,12 +1,13 @@
+
 import java.util.Scanner;
 
 public class Input {
-    public static void main(String[] args){
 
-        
+    public static void main(String[] args) {
+
         System.out.println("Enter your name:");
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine(); 
+        String name = sc.nextLine();
         System.out.println("Hi " + name);
     }
 }
