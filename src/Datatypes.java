8b4ebe9374f6715981ec8033de84f3165a445e35
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:16:15 +0530

    differnt types of datatype in java

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -7,7 +7,7 @@ public class Datatypes {
         // bytes - 1 [-128 to 127]
         // short - 2 
         // int - 4
-        // long int - 8 
+        // long int - 8  
         // float - 4
         // double - 8  
         // char - 2
