948624875f042d5e8c00cae6c18dde43ad226d3b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:11:39 +0530

    using random method finding or guessing the number

--- src/project.java
+++ src/project.java
@@ -1,37 +1,30 @@
 
 import java.util.Scanner;
 
-
-
 public class project {
-    
-    // Mini projects
 
-    public static void main(String[] args){
-        
+    // Mini projects
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int myNum = (int)(Math.random()*100);
+        int myNum = (int) (Math.random() * 100);
         int userNum;
-       
-        do { 
-              System.out.println("Guess my number (1-100):");
-               userNum = sc.nextInt();
-              if(userNum == myNum){
+
+        do {
+            System.out.println("Guess my number (1-100):");
+            userNum = sc.nextInt();
+            if (userNum == myNum) {
                 System.out.println("Woah! You guessed it right");
                 break;
-              }
-              else if(userNum > myNum) {
+            } else if (userNum > myNum) {
 
                 System.out.println("Your number is too large");
-              }
-
-              else {
+            } else {
                 System.out.println("Your number is too small");
-              }
+            }
 
-        } 
-          while (userNum >= 0);
+        } while (userNum >= 0);
 
-          System.out.println("My number was: " + myNum);
+        System.out.println("My number was: " + myNum);
     }
 }
