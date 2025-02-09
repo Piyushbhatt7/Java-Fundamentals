2974823d916f926ef4de5858047f1a1a568a43e1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-09 23:08:27 +0530

    calculator in java

--- src/calculator.java
+++ src/calculator.java
@@ -1,10 +1,10 @@
 
 import java.util.Scanner;
 
-public class calculator{
+public class calculator {
 
     public static void main(String[] args) {
-        
+
         int num1, num2;
         double sum;
         String operator;
@@ -23,38 +23,27 @@ public class calculator{
         Scanner sc3 = new Scanner(System.in);
         operator = sc3.nextLine();
 
-        
-        if(operator.equals("+")){
+        if (operator.equals("+")) {
 
             sum = num1 + num2;
             System.out.println("Sum of " + num1 + " and " + num2 + " is = " + sum);
-        }
-
-        else if(operator.equals("-")){
+        } else if (operator.equals("-")) {
 
             sum = num1 - num2;
             System.out.println("Difference of " + num1 + " and " + num2 + " is = " + sum);
-        }
-
-        else if(operator.equals("*")){
+        } else if (operator.equals("*")) {
 
             sum = num1 * num2;
             System.out.println("Multiplication of " + num1 + " and " + num2 + " is = " + sum);
-        }
-
-        else if(operator.equals("/")){
+        } else if (operator.equals("/")) {
 
             sum = num1 / num2;
             System.out.println("Division of " + num1 + " and " + num2 + " is = " + sum);
-        }
-
-        else if(operator.equals("%")){
+        } else if (operator.equals("%")) {
 
             sum = num1 % num2;
             System.out.println("Modulor of " + num1 + " and " + num2 + " is = " + sum);
-        }
-
-        else{
+        } else {
 
             System.out.println("Wrong input!!");
         }
